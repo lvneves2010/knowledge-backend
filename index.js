4499407d90e0b20c397504291fab65c48d59e630
Leonardo <lvneves2010@gmail.com>
2020-05-01 18:54:51 -0300

    middleware

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const app = require('express')()
+const consign = require('consign')
+
+consign() 
+    .then( './config/middleware.js' )
+    .into(app)
 
 app.listen(3000, () => {
     console.log( 'Backend rodando....' )
