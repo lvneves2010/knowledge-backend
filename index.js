65ddc3a3c1e45380214da311efdeba0321d07916
Leonardo <lvneves2010@gmail.com>
2020-05-01 19:13:57 -0300

    rota user

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const consign = require('consign')
 
 consign() 
     .then( './config/middleware.js' )
+    .then( './api' )
+    .then( './config/routes.js' )
     .into(app)
 
 app.listen(3000, () => {
