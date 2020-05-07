6402c7d7d1147d36eb5411a3de24f5e4c2ce27fc
Leonardo <lvneves2010@gmail.com>
2020-05-07 15:21:17 -0300

    atualizacoes seguranca a cesso

--- api/user.js
+++ api/user.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt-nodejs')
+
 module.exports = app => {
     const { existsOrError, notExistsOrError, equalsOrError } = app.api.validation
 
@@ -10,6 +11,9 @@ module.exports = app => {
     const save = async (req, res) => {
         const user = { ...req.body }
         if(req.params.id) user.id = req.params.id
+
+        if(!req.originalUrl.startsWith( '/users' )) user.admin = false
+        if(!req.user || !req.user.admin) user.admin = false
 
         try {
             existsOrError(user.name, 'Nome não informado' )
@@ -34,6 +38,7 @@ module.exports = app => {
             app.db('users')
                 .update(user)
                 .where({ id: user.id })
+                .whereNull( 'deletedAt' )
                 .then(_ => res.status(204).send())
                 .catch(err => res.status(500).send(err))
         } else {
@@ -48,6 +53,7 @@ module.exports = app => {
     const get = ( req, res ) => {
         app.db( 'users' )
             .select( 'id', 'name', 'email', 'admin' )
+            .whereNull( 'deletedAt' )
             .then( users => res.json(users) )
             .catch( err => res.status(500).send(err) )
     }
@@ -56,10 +62,28 @@ module.exports = app => {
         app.db( 'users' )
             .select( 'id', 'name', 'email', 'admin' )
             .where({ id: req.params.id })
+            .whereNull( 'deletedAt' )
             .first()
             .then( user => res.json(user) )
             .catch( err => res.status(500).send(err) )
     }
 
-    return { save, get, getById }
+    const remove = async ( req, res ) => {
+        try {
+            const articles = await app.db( 'articles' )
+                .where({ userId: req.params.id })
+            notExistsOrError(articles, 'Usuarios possui Artigos')
+
+            const rowsUdated = await app.db( 'users' )
+                .update({ deletedAt: new Date() })
+                .where({ id: req.params.id })
+            existsOrError(rowsUdated, 'Usuário não foi encontrado')
+
+            res.status(204).send()
+        } catch(msg) {
+            res.status(400).send(msg)
+        }
+    }
+
+    return { save, get, getById, remove }
 }
