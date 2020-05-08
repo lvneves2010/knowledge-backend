79a4e3a0362a85d81c1f641d1dbd4d5b3cfd3332
Leonardo <lvneves2010@gmail.com>
2020-05-07 22:53:54 -0300

    mongo remoto + postgree local

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,6 +1,18 @@
 const mongoose = require( 'mongoose' )
-mongoose.connect( 'mongodb://localhost/knowledge_stats', { useNewUrlParser: true } )
-    .catch(e => {
-        const msg = 'Erro!! não foi possível conectar com o MongoDB!!'
-        console.log( '\x1b[41m%s\x1b[37m', msg, '\x1b[0m' )
-    } )  
+
+// conexão local
+// mongoose.connect( 'mongodb://localhost/knowledge_stats', { useNewUrlParser: true } )
+//     .catch(e => {
+//         const msg = 'Erro!! não foi possível conectar com o MongoDB!!'
+//         console.log( '\x1b[41m%s\x1b[37m', msg, '\x1b[0m' )
+//     } ) 
+
+
+// modelo do cluster
+// mongodb+srv://lvneves2010:<password>@cluster0-m9sca.mongodb.net/test?retryWrites=true&w=majority    
+
+
+// conexão no mongo atlas
+mongoose.connect('mongodb+srv://lvneves2010:Gustavo0801@cluster0-m9sca.mongodb.net/knowledge?retryWrites=true&w=majority', { useNewUrlParser: true } )
+ .then(() =>  console.log('connection succesful'))
+ .catch((err) => console.error(err));
--- knexfile.js
+++ knexfile.js
@@ -1,8 +1,10 @@
-const { db } = require( './.env' )
+const { dbheroku, dblocal } = require( './.env' )
 
 module.exports = {
     client: 'postgresql',
-    connection: db,
+    connection: dblocal,
+    // connection: 'postgres://jkwarzjwcnsjmu:32e033ab173a38cb7c0cc61ab6d6f6ade7cd22c32034603ceb46f91818140cb3@ec2-34-234-228-127.compute-1.amazonaws.com:5432/dc0a08uagqcrkb',
+    // connection: dbheroku,
     pool: {
       min: 2,
       max: 10
